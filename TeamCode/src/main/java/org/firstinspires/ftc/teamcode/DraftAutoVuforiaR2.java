ff4b974320e38d6d41fe3664ab96e4b0638154dd
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2017-12-06 16:18:40 -0500

    Pasted New Vuforia Code
    Pasted the new vuforia code into 3 of the autonomous drafts:
    B1
    B2
    R2
    The old version was reverted for easier uninstallment of unneeded code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DraftAutoVuforiaR2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DraftAutoVuforiaR2.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 /**
  * Created by justin on 12/1/17.
  */
@@ -10,6 +16,29 @@ public class DraftAutoVuforiaR2 extends AutonomousMethodMaster{
     {
         initElectronics(0);
 
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+        // the licence key that vuforia needs to work
+        parameters.vuforiaLicenseKey = "AQRacK7/////AAAAGea1bsBsYEJvq6S3KuXK4PYTz4IZmGA7SV88bdM7l26beSEWkZTUb8H352Bo/ZMC6krwmfEuXiK7d7qdFkeBt8BaD0TZAYBMwHoBkb7IBgMuDF4fnx2KiQPOvwBdsIYSIFjiJgGlSj8pKZI+M5qiLb3DG3Ty884EmsqWQY0gjd6RNhtSR+6oiXazLhezm9msyHWZtX5hQFd9XoG5npm4HoGaZNdB3g5YCAQNHipjTm3Vkf71rG/Fffif8UTCI1frmKYtb4RvqiixDSPrD6OG6YmbsPOYUt2RZ6sSTreMzVL76CNfBTzmpo2V0E6KKP2y9N19hAum3GZu3G/1GEB5D+ckL/CXk4JM66sJw3PGucCs";
+
+        // indicates camera direction
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+
+        // loads data for vumarks
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+        waitForStart();
+
+        relicTrackables.activate();
+
+        RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
+
         waitForStart();
 
         // red1
@@ -17,6 +46,29 @@ public class DraftAutoVuforiaR2 extends AutonomousMethodMaster{
 
         int move_inches = 0;
         // identify which vumark
+        while (vuMark == null){
+            //motorL.setPower(0.25);
+            //motorR.setPower(0.25);
+            vuMark = RelicRecoveryVuMark.from(relicTemplate);
+        }
+        if(vuMark == RelicRecoveryVuMark.LEFT){
+            telemetry.addData("VuMark", "%s visible", vuMark);
+            telemetry.update();
+
+            move_inches = 2;
+        }
+        if(vuMark == RelicRecoveryVuMark.CENTER){
+            telemetry.addData("VuMark", "%s visible", vuMark);
+            telemetry.update();
+
+            move_inches = 6;
+        }
+        if(vuMark == RelicRecoveryVuMark.RIGHT){
+            telemetry.addData("VuMark", "%s visible", vuMark);
+            telemetry.update();
+
+            move_inches = 10;
+        }
 
         encoderRotateDegrees(1, 1, 90);
 
