87c823d35c3b1cc698ace4565cabece99e419bc6
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2017-12-08 15:42:04 -0500

    Reversed Motors
    Reversed 2 motors - FL and FR.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop.java
@@ -54,8 +54,11 @@ public class BasicTeleop extends OpMode
         //motorL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
 
         // Flipped the motors (11/10/17)
-        motorFR.setDirection(DcMotor.Direction.REVERSE);
+        motorFR.setDirection(DcMotor.Direction.REVERSE);            //Because the motors rotate CCW, the right motors need to be reversed
         motorBR.setDirection(DcMotor.Direction.REVERSE);
+        motorFL.setDirection(DcMotor.Direction.REVERSE);            //The front motors are reversed because of the gears on the back motors
+        motorFR.setDirection(DcMotor.Direction.REVERSE);
+
         motorFlyL.setDirection(DcMotor.Direction.REVERSE);
         //motorL.setDirection(DcMotorSimple.Direction.REVERSE);
         /* ---------------------------------------- */
