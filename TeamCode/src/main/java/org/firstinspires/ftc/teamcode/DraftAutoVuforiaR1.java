fdd8088ee6ed82fad53e540c86453842736e7b06
justin <jzhu2020@k12.andoverma.us>
2017-12-20 16:50:42 -0500

    Drafted vuforia based positioning in R1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DraftAutoVuforiaR1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DraftAutoVuforiaR1.java
@@ -10,10 +10,19 @@ import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 import com.qualcomm.robotcore.hardware.SwitchableLight;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.RelicRecoveryVuMark;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+import static com.sun.tools.javac.util.Constants.format;
 
 /**
  * Created by justin on 12/1/17.
@@ -93,13 +102,13 @@ public class DraftAutoVuforiaR1 extends AutonomousMethodMaster{
 
             move_inches = 4;
         }
-        if(vuMark == RelicRecoveryVuMark.CENTER){
+        else if(vuMark == RelicRecoveryVuMark.CENTER){
             telemetry.addData("VuMark", "%s visible", vuMark);
             telemetry.update();
 
             move_inches = 12;
         }
-        if(vuMark == RelicRecoveryVuMark.RIGHT) {
+        else if(vuMark == RelicRecoveryVuMark.RIGHT) {
             telemetry.addData("VuMark", "%s visible", vuMark);
             telemetry.update();
 
@@ -111,6 +120,39 @@ public class DraftAutoVuforiaR1 extends AutonomousMethodMaster{
             telemetry.update();
 
             move_inches = 12;
+        }
+        OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
+        telemetry.addData("Pose", format(pose));
+
+        /* We further illustrate how to decompose the pose into useful rotational and
+         * translational components */
+        boolean first = false;
+        double tX = 0, tY = 0, tZ = 0;
+        while (pose != null && (!first || tX < 20)) // 20 as in 20 inches
+        {
+            first = true;
+            VectorF trans = pose.getTranslation();
+            Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
+
+            // Extract the X, Y, and Z components of the offset of the target relative to the robot
+            tX = trans.get(0);
+            tY = trans.get(1);
+            tZ = trans.get(2);
+
+            // Extract the rotational components of the target relative to the robot
+            double rX = rot.firstAngle;
+            double rY = rot.secondAngle;
+            double rZ = rot.thirdAngle;
+
+            telemetry.addData("X translation", tX);
+            telemetry.addData("Y translation", tY);
+            telemetry.addData("Z translation", tZ);
+
+            telemetry.addData("X rotation", rX);
+            telemetry.addData("Y rotation", rY);
+            telemetry.addData("Z rotation", rZ);
+
+            encoderMove(1,0.1,0.1); // just move...
         }
 
         /*
@@ -127,6 +169,10 @@ public class DraftAutoVuforiaR1 extends AutonomousMethodMaster{
         encoderMove(.5, 4,4);
 
         dumpGlyph(); // dump the glyph
+    }
+
+    String format(OpenGLMatrix transformationMatrix) {
+        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
     }
 
 }
